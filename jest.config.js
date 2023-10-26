16ec904d7ffb8b9f9992b5bc8ce37e29e969449a
rizkyzhang <rizkyzhangdev@gmail.com>
2023-10-26 19:48:42 +0700

    chore: add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverageFrom: ["src/**/*.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/$1",
+  },
+  moduleDirectories: ["node_modules", "src"],
+  modulePathIgnorePatterns: ["dist"],
+};
